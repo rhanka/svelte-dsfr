6afe5094bf4446e821605cb3827f3f3f0bc63e78
antoinefa <fabien.antoine@m4x.org>
2021-11-21 23:08:32 +0100

    add Button

---
+++ src/lib/Button/Button.svelte
@@ -0,0 +1,52 @@
+<button
+    class="fr-btn inline-flex"
+    class:fr-btn--secondary={secondary}
+    class:reverse={iconRight}
+    disabled={disabled}
+    aria-disabled={disabled}
+>
+    {#if icon}
+      <Icon
+        icon="icon"
+        class={iconRight ? "icon-right" : "icon-left"}
+      />
+    {/if}
+
+    <span>
+      { label }
+      <slot />
+    </span>
+</button>
+
+<script lang="ts">
+export let disabled: boolean;
+export let label: string;
+export let secondary: boolean;
+export let icon: string;
+export let iconRight: boolean;
+
+import '@gouvfr/dsfr/dist/scheme/scheme.css';
+import '@gouvfr/dsfr/dist/core/core.css';
+import '@gouvfr/dsfr/dist/component/form/form.css';
+import '@gouvfr/dsfr/dist/component/link/link.css';
+import '@gouvfr/dsfr/dist/component/button/button.css';
+</script>
+
+<style>
+
+.icon-left {
+  margin-right: 0.5rem
+}
+.icon-right {
+  margin-left: 0.5rem
+}
+
+.inline-flex {
+  display: inline-flex;
+  align-items: center;
+}
+
+.reverse {
+  flex-direction: row-reverse;
+}
+</style>
