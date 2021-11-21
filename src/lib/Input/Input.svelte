e11d1cf2a5e8f81e5529b28b5d7ac2ecd5ee0b66
antoinefa <fabien.antoine@m4x.org>
2021-11-21 23:08:44 +0100

    add Input

---
+++ src/lib/Input/Input.svelte
@@ -0,0 +1,42 @@
+<label
+    class="fr-label"
+    class:invisible={!labelVisible}
+    for={id}
+>
+    {label}
+</label>
+<input
+    {id}
+    {type}
+    class="fr-input"
+    {placeholder}
+    {value}
+    on:input={handleInput}
+>
+
+<script lang="ts">
+  import '@gouvfr/dsfr/dist/scheme/scheme.css';
+  import '@gouvfr/dsfr/dist/core/core.css';
+  import '@gouvfr/dsfr/dist/component/form/form.css';
+  import '@gouvfr/dsfr/dist/component/link/link.css';
+  import '@gouvfr/dsfr/dist/component/button/button.css';
+  import '@gouvfr/dsfr/dist/component/input/input.css';
+
+  import uuid from 'uuid-random';
+
+  export let id: string = uuid();
+  export let label: string = '';
+  export let labelVisible: boolean;
+  export let value: string = '';
+  export let placeHolder: string = '';
+  export let type: string = 'text';
+
+  const handleInput = e => {
+    value = type.match(/^(number|range)$/)
+      ? +e.target.value
+      : e.target.value;
+  };
+</script>
+
+<style>
+</style>
