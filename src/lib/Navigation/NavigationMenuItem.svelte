1e9a1a27cd509728c57a6c56cf2742b1e02ed420
antoinefa <fabien.antoine@m4x.org>
2022-01-01 22:05:49 +0100

    add navigation menu item

---
+++ src/lib/Navigation/NavigationMenuItem.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import '@gouvfr/dsfr/dist/scheme/scheme.css'
+  import '@gouvfr/dsfr/dist/core/core.css'
+  import '@gouvfr/dsfr/dist/component/navigation/navigation.css'
+
+  import uuid from 'uuid-random'
+
+  export let id: string = uuid()
+  export let active: boolean = false
+</script>
+
+<li
+  id={id}
+  class="fr-menu__item"
+  aria-current={active ? 'page' : undefined}
+>
+  <slot />
+</li>
