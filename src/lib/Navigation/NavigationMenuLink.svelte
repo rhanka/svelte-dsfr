591a2d32cb96c88907e5c45c669645f53d71a32b
antoinefa <fabien.antoine@m4x.org>
2022-01-01 21:44:07 +0100

    add navigation menu link

---
+++ src/lib/Navigation/NavigationMenuLink.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import '@gouvfr/dsfr/dist/scheme/scheme.css'
+  import '@gouvfr/dsfr/dist/core/core.css'
+  import '@gouvfr/dsfr/dist/component/navigation/navigation.css'
+
+  import uuid from 'uuid-random'
+
+  export let id: string = uuid()
+  export let to: any
+  export let text: string
+</script>
+
+<a
+  class="fr-nav__link"
+  href={to}
+>
+  { text }
+</a>
