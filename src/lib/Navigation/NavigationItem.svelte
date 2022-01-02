5f15cfe9690fcc57947e34a12dcb9b9f810f3aba
antoinefa <fabien.antoine@m4x.org>
2022-01-02 13:35:10 +0100

    add navigation item

---
+++ src/lib/Navigation/NavigationItem.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import '@gouvfr/dsfr/dist/scheme/scheme.css'
+  import '@gouvfr/dsfr/dist/core/core.css'
+  import '@gouvfr/dsfr/dist/component/navigation/navigation.css'
+
+  import uuid from 'uuid-random'
+
+  export let id: string = uuid()
+  export let active: boolean = false
+</script>
+
+<li
+  id={id}
+  class="fr-nav__item"
+  aria-current={active ? 'page' : undefined}
+>
+  <!-- @slot Slot par défaut pour le contenu de l’item de liste. Sera dans `<li class="fr-nav__item">` -->
+  <slot />
+</li>
