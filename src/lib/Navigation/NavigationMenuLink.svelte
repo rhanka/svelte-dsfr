1ae5cc14c59aaea6a17f32b7847839a2bcf3f064
antoinefa <fabien.antoine@m4x.org>
2022-01-02 13:38:01 +0100

    add icon handling in NavigationMenuLink

--- src/lib/Navigation/NavigationMenuLink.svelte
+++ src/lib/Navigation/NavigationMenuLink.svelte
@@ -7,12 +7,36 @@
 
   export let id: string = uuid()
   export let to: any
-  export let text: string
+  export let label: string
+  export let icon: string
+  export let iconRight: boolean
 </script>
 
 <a
-  class="fr-nav__link"
   href={to}
+  class="fr-nav__link"
+  class:reverse={iconRight}
+  target={to.startsWith('http') ? '_blank' : ''}
 >
-  { text }
+  {#if icon}
+    <span
+      class="icon"
+      class:fr-ml-1w={label && iconRight}
+      class:fr-mr-1w={label && !iconRight}
+    >
+      <Icon {icon}/>
+    </span>
+  {/if}
+  {#if label}
+    {label}
+  {/if}
 </a>
+
+<style scoped>
+  .icon {
+    margin-bottom: -0.25rem;
+  }
+  .reverse {
+    flex-direction: row-reverse;
+  }
+</style>
