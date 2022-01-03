2ee338a4bceff69a98e0feebabc9b95b26a02aa6
antoinefa <fabien.antoine@m4x.org>
2022-01-03 12:29:45 +0100

    enable theme switching in DisplayParamsModal

--- src/lib/DisplayParams/DisplayParamsModal.svelte
+++ src/lib/DisplayParams/DisplayParamsModal.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+	export const ssr = false;
+</script>
+
 <script lang="ts">
   import '@gouvfr/dsfr/dist/scheme/scheme.css'
   import '@gouvfr/dsfr/dist/core/core.css'
@@ -10,7 +14,25 @@
   import lightSVG from '@gouvfr/dsfr/dist/artwork/light.svg?url'
   import systemSVG from '@gouvfr/dsfr/dist/artwork/system.svg?url'
 
-  import { session } from '$app/stores';
+  import { session } from '$app/stores'
+  import { onMount } from 'svelte'
+
+  let theme: string;
+
+  let html: any
+
+  onMount(() => {
+    html = document.documentElement
+    theme = html.data-fr-theme || html.data-fr-scheme || 'system'
+  })
+
+
+  $: console.log(theme)
+
+  $: if (html && theme) {
+    html.setAttribute("data-fr-theme", theme)
+    html.setAttribute("data-fr-scheme", theme)
+  }
 
 </script>
 
@@ -30,7 +52,7 @@
             <button
               class="fr-link--close fr-link"
               aria-controls="fr-theme-modal"
-              on:click={() => $session.displayParamsModal = !$session.displayParamsModal}
+              on:click={() => $session.displayParamsModal = false}
             >
               Fermer
             </button>
@@ -40,43 +62,43 @@
                 Paramètres d’affichage
             </h1>
             <div id="fr-display" class="fr-form-group fr-display">
-                <div class="fr-form-group">
-                    <fieldset class="fr-fieldset">
-                        <legend class="fr-fieldset__legend fr-text--regular" id='-legend'>
-                            Choisissez un thème pour personnaliser l’apparence du site.
-                        </legend>
-                        <div class="fr-fieldset__content">
-                            <div class="fr-radio-group fr-radio-rich">
-                                <input value="light" type="radio" id="fr-radios-theme-light" name="fr-radios-theme">
-                                <label class="fr-label" for="fr-radios-theme-light">Thème clair
-                                </label>
-                                <div class="fr-radio-rich__img" data-fr-inject-svg>
-                                    <img src={lightSVG} alt="" />
-                                    <!-- L’alternative de l’image (attribut alt) doit rester vide car l’image est illustrative et ne doit pas être restituée aux technologies d’assistance -->
-                                </div>
-                            </div>
-                            <div class="fr-radio-group fr-radio-rich">
-                                <input value="dark" type="radio" id="fr-radios-theme-dark" name="fr-radios-theme">
-                                <label class="fr-label" for="fr-radios-theme-dark">Thème sombre
-                                </label>
-                                <div class="fr-radio-rich__img" data-fr-inject-svg>
-                                    <img src={darkSVG} alt="" />
-                                    <!-- L’alternative de l’image (attribut alt) doit rester vide car l’image est illustrative et ne doit pas être restituée aux technologies d’assistance -->
-                                </div>
-                            </div>
-                            <div class="fr-radio-group fr-radio-rich">
-                                <input value="system" type="radio" id="fr-radios-theme-system" name="fr-radios-theme">
-                                <label class="fr-label" for="fr-radios-theme-system">Système
-                                    <span class="fr-hint-text">Utilise les paramètres système.</span>
-                                </label>
-                                <div class="fr-radio-rich__img" data-fr-inject-svg>
-                                    <img src={systemSVG} alt="" />
-                                    <!-- L’alternative de l’image (attribut alt) doit rester vide car l’image est illustrative et ne doit pas être restituée aux technologies d’assistance -->
-                                </div>
-                            </div>
-                        </div>
-                    </fieldset>
-                </div>
+              <div class="fr-form-group">
+                <fieldset class="fr-fieldset">
+                  <legend class="fr-fieldset__legend fr-text--regular" id='-legend'>
+                    Choisissez un thème pour personnaliser l’apparence du site.
+                  </legend>
+                  <div class="fr-fieldset__content">
+                    <div class="fr-radio-group fr-radio-rich">
+                      <input value="light" bind:group={theme} type="radio" id="fr-radios-theme-light" name="fr-radios-theme">
+                      <label class="fr-label" for="fr-radios-theme-light">Thème clair
+                      </label>
+                      <div class="fr-radio-rich__img" data-fr-inject-svg>
+                        <img src={lightSVG} alt="" />
+                        <!-- L’alternative de l’image (attribut alt) doit rester vide car l’image est illustrative et ne doit pas être restituée aux technologies d’assistance -->
+                      </div>
+                    </div>
+                    <div class="fr-radio-group fr-radio-rich">
+                      <input value="dark" bind:group={theme} type="radio" id="fr-radios-theme-dark" name="fr-radios-theme">
+                      <label class="fr-label" for="fr-radios-theme-dark">Thème sombre
+                      </label>
+                      <div class="fr-radio-rich__img" data-fr-inject-svg>
+                        <img src={darkSVG} alt="" />
+                        <!-- L’alternative de l’image (attribut alt) doit rester vide car l’image est illustrative et ne doit pas être restituée aux technologies d’assistance -->
+                      </div>
+                    </div>
+                    <div class="fr-radio-group fr-radio-rich">
+                      <input value="system" bind:group={theme} type="radio" id="fr-radios-theme-system" name="fr-radios-theme">
+                      <label class="fr-label" for="fr-radios-theme-system">Système
+                          <span class="fr-hint-text">Utilise les paramètres système.</span>
+                      </label>
+                      <div class="fr-radio-rich__img" data-fr-inject-svg>
+                          <img src={systemSVG} alt="" />
+                          <!-- L’alternative de l’image (attribut alt) doit rester vide car l’image est illustrative et ne doit pas être restituée aux technologies d’assistance -->
+                      </div>
+                    </div>
+                  </div>
+                </fieldset>
+              </div>
             </div>
           </div>
         </div>
