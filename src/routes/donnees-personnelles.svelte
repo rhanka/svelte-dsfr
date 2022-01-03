141358996d1bacc6e34bb08188c9202b954419da
antoinefa <fabien.antoine@m4x.org>
2022-01-03 14:51:46 +0100

    add fake donnees-personnelles

---
+++ src/routes/donnees-personnelles.svelte
@@ -0,0 +1,7 @@
+<h1 class="fr-mt-3w">Données personnelles</h1>
+
+A compléter ...
+
+Ceci dit, aucune donnée personnelle n'est collectée directement par le composant svelte-dsfr,
+ni par le présent site.
+
