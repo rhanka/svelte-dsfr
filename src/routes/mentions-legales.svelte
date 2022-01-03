2a17dd446ab86d5b0513a3bee3df4fd2e9a867ca
antoinefa <fabien.antoine@m4x.org>
2022-01-03 14:36:17 +0100

    add fake mention-legales

---
+++ src/routes/mentions-legales.svelte
@@ -0,0 +1,54 @@
+<h1 class="fr-mt-3w">Mentions légales</h1>
+
+<h2>Éditeur de la Plateforme</h2>
+
+<p>
+  Le site est éditée Fabien ANTOINE, auteur de svelte-dsfr.
+</p>
+
+<p>
+  Coordonnées : svelte-dsfr@gmail.com
+</p>
+
+<h2>Directrice.eur de la publication</h2>
+
+  Fabien ANTOINE
+
+<h2>Hébergement de la Plateforme</h2>
+  Ce site est hébergé sur Github Pages.
+
+<h2>Accessibilité</h2>
+<p>La conformité aux normes d’accessibilité numérique est un objectif ultérieur. En attendant, nous tâchons de rendre ce site
+accessible à toutes et à tous :</p>
+<ul>
+  <li>Utilisation de composants accessibles (design system de l'État)</li>
+  <li>Respect des bonnes pratiques (Pilida, Opquast...)</li>
+  <li>Tests manuels</li>
+</ul>
+
+<h3>Signaler un dysfonctionnement</h3>
+<p>
+  Si vous rencontrez un défaut d’accessibilité vous empêchant d’accéder à un contenu ou une fonctionnalité du site,
+  merci de nous en faire part en
+  <a title="Contactez-nous" href="mailto:svelte-dsfr@gmail.com" target="_blank" rel="noopener noreferrer">
+    nous contactant à l'adresse svelte-dsfr@gmail.com
+  </a>.
+</p>
+<p>
+  Si vous n’obtenez pas de réponse rapide de notre part,
+  vous êtes en droit de faire parvenir vos doléances ou une demande de saisine au Défenseur des droits.
+</p>
+
+<h3>En savoir plus</h3>
+    Pour en savoir plus sur la politique d’accessibilité numérique de l’État :
+    <a href="http://references.modernisation.gouv.fr/accessibilite-numerique">http://references.modernisation.gouv.fr/accessibilite-numerique</a>
+
+<h2>Sécurité</h2>
+
+<p>
+  Le site est protégé par un certificat électronique, matérialisé pour la grande majorité des navigateurs par un cadenas.
+    Cette protection participe à la confidentialité des échanges.
+    En aucun cas les services associés à la plateforme ne seront à l’origine d’envoi de courriels pour demander
+    la saisie d’informations personnelles.
+</p>
+
